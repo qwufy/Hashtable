cc4b790f57a662d4f9ad8c03433573421c364480
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-13 17:18:13 +0600

    assignment4

---
+++ HashTable/src/myHashTable.java
@@ -0,0 +1,37 @@
+public class myHashTable<K, V> {
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString(){
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    public myHashTable(){...}
+
+    public myHashTable(int M){...}
+
+    private int hash(K key) {...}
+
+    public void put(K key, V value){...}
+
+    public V get(K key){...}
+
+    public V remove(K key){...}
+
+    public boolean contains(V value){...}
+
+    public K getKey(V value){...}
+}
