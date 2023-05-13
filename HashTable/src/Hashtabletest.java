769f225b05c303e5b88c4024c0dd4c074ffc0131
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-13 17:58:01 +0600

    assignment4

--- HashTable/src/Hashtabletest.java
+++ HashTable/src/Hashtabletest.java
@@ -2,28 +2,16 @@ public class Hashtabletest {
     private int id;
     private String name;
 
-    public  Hashtabletest(int id, String name){
+    public Hashtabletest(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
     @Override
-    public int hashCode(){
-        int result = 17;
-        result = 31 * result + id;
+    public int hashCode() {
+        int result = Integer.hashCode(id);
         result = 31 * result + name.hashCode();
         return result;
     }
-
-    @Override
-    public  boolean equals(Object o){
-        if (o == this){
-            return true;
-        }
-        if(!(o instanceof Hashtabletest)){
-            return false;
-        }
-        Hashtabletest other = (Hashtabletest) o;
-        return id == other.id && name.equals(other.name);
-    }
 }
+
